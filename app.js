f38c5edf2813ee7720b5a1607e6ba3e936f561ff
JNYLee <junyjuny1021@gmail.com>
2021-11-08 15:50:21 +0900

    [showBrushLine/init]: 캔버스에 그린 선 보이게 하기

--- app.js
+++ app.js
@@ -1,34 +1,36 @@
+const canvas = document.getElementById("jsCanvas");
+const ctx = canvas.getContext("2d");
+
+canvas.width = 700;
+canvas.height = 700;
+
+ctx.strokeStyle = "#2c2c2c";
+ctx.lineWidth = 2.5;
+
+let painting = false;
 function stopPainting() {
   painting = false;
 }
-
+function startPainting() {
+  painting = true;
+}
 function onMouseMove(event) {
   const x = event.offsetX;
   const y = event.offsetY;
-}
-
-function onMouseDown(event) {
-  painting = true;
-}
-
-function onMouseUp(event) {
-  stopPainting();
-}
-
-function mouseInCanvasEventListener() {
-    canvas.addEventListener("mousemove", onMouseMove);
-    canvas.addEventListener("mousedown", onMouseDown);
-    canvas.addEventListener("mouseup", onMouseUp);
-    canvas.addEventListener("mouseleave", stopPainting);
-}
-
-document.addEventListener("DOMContentLoaded", () => {
-  const canvas = document.getElementById("jsCanvas");
-
-  let painting = false;
-
-  if (canvas) {
-      mouseInCanvasEventListener();
+  if (!painting) {
+    ctx.beginPath();
+    ctx.moveTo(x, y);
+  } else {
+    ctx.lineTo(x, y);
+    ctx.stroke();
   }
-
-});
+}
+// function onMouseDown(event) {
+//   painting = true;
+// }
+if (canvas) {
+  canvas.addEventListener("mousemove", onMouseMove);
+  canvas.addEventListener("mousedown", startPainting);
+  canvas.addEventListener("mouseup", stopPainting);
+  canvas.addEventListener("mouseleave", stopPainting);
+}
